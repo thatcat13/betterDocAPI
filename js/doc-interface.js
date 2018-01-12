085c9c97cb109fd52d11f6e6cba64d8225f7eb26
cj <thatcat13@gmail.com>
2018-01-12 09:53:14 -0800

    able to obtain properties on object but url returns an empty array with no errors called

--- js/doc-interface.js
+++ js/doc-interface.js
@@ -1,1 +1,21 @@
 const apiKey = require('./../.env').apiKey;
+import { FindDoc } from './../js/logic.js';
+
+$(document).ready(function(){
+
+  $('#search-form').submit(function(event){
+    event.preventDefault();
+    const medical = $('#medical-input').val();
+    const name = $('#name-input').val();
+    const location = $('#location-input').val();
+    const newRequest = new FindDoc(medical, name, location);
+    console.log(newRequest.inputMedical);
+    console.log(newRequest.inputName);
+    console.log(newRequest.inputLocation);
+
+    console.log(newRequest.getDoc());
+
+
+
+  });//preventDefault
+});//document ready
--- js/logic.js
+++ js/logic.js
@@ -1,1 +1,37 @@
 const apiKey = require('./../.env').apiKey;
+
+
+export class FindDoc {
+  constructor(inputLocation, inputMedical, inputName) {
+    this.inputLocation = inputLocation;
+    this.inputMedical = inputMedical;
+    this.inputName = inputName;
+  }
+
+  getDoc(){//success and error are callback function names
+    $.ajax({
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${this.inputName}&query=${this.inputMedical}&location=${this.inputLocation}&skip=0&limit=10&user_key=dfd7d85b06d20bac4aede6a598829871`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+
+      success: function(response) {
+        console.log(response);
+        // const resultsArray = response.map(function(medicalIssue){
+        //   let issue = {};
+        //   issue[medicalIssue.key] = response.data.practices.;
+        //   return issue;
+      },
+        // resultsArray.map(response.data[i].images.fixed_height.url);
+        // }
+
+        // success(newArray);//callback function named success with ARGUMENT response that hasn't necessarily been grabbed yet from server
+
+      error: function() {
+        // error(response);//callback function named error that's a blank template for doing whatever we want when called in frontend
+      }
+
+    });//ajax
+  }//getDoc
+} //FindDoc
