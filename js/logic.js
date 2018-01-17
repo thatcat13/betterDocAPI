2a82b213068e617898070e662cd3875dcd08162f
cj <thatcat13@gmail.com>
2018-01-16 17:21:26 -0800

    begin assigning constants with json parsed data

--- js/logic.js
+++ js/logic.js
@@ -7,7 +7,6 @@ export class FindDoc {
     this.inputMedical = inputMedical;
   }
 
-// https://api.betterdoctor.com/2016-03-01/doctors?query=${this.inputMedical}&sort=full-name-asc&skip=0&limit=50&user_key=${apiKey}
   getDoc(success, error){//success and error are callback function names
     $.ajax({
       url: `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${this.inputName}&query=${this.inputMedical}&location=or-portland&user_location=45.5231,-122.6765&skip=0&limit=50&user_key=${apiKey}`,
@@ -20,9 +19,16 @@ export class FindDoc {
         console.log(response);
         let resultsArray = [];
         for (var i = 0; i < response.data.length; i++){
-          if (response.data[i].practices[0])  {
-            resultsArray.push(response.data[i].profile.last_name + ', ' + response.data[i].profile.first_name + '<br>' + 'Accepting new patients: ' +  response.data[i].practices[0].accepts_new_patients + '<br>' + 'Address: ' + response.data[i].practices[0].visit_address.street + ' ' + response.data[i].practices[0].visit_address.city + ', ' + response.data[i].practices[0].visit_address.state + '<br>' + 'Phone number: ' + response.data[i].practices[0].phones[0].number + '<br>'+ 'Website: ' + response.data[i].practices[0].website);
-          }
+          const docName = response.data[i].profile.last_name + ', ' + response.data[i].profile.first_name;
+          const docStreetAddress = response.data[i].practices[0].visit_address.street;
+          const docCityAddress = response.data[i].practices[0].visit_address.city;
+          const docStateAddress = response.data[i].practices[0].visit_address.state;
+
+          console.log(docName);
+          console.log(docStreetAddress);
+          console.log(docCityAddress);
+          console.log(docStateAddress);
+          // console.log(docFirstName);
         }
 
 
@@ -38,3 +44,7 @@ export class FindDoc {
     });//ajax
   }//getDoc
 } //FindDoc
+
+// if (response.data[i].practices[0]) {
+//   resultsArray.push(response.data[i].profile.last_name + ', ' + response.data[i].profile.first_name + '<br>' + 'Accepting new patients: ' +  response.data[i].practices[0].accepts_new_patients + '<br>' + 'Address: ' + response.data[i].practices[0].visit_address.street + ' ' + response.data[i].practices[0].visit_address.city + ', ' + response.data[i].practices[0].visit_address.state + '<br>' + 'Phone number: ' + response.data[i].practices[0].phones[0].number + '<br>'+ 'Website: ' + response.data[i].practices[0].website);
+// }
