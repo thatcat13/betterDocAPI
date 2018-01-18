dc4ecdc5c873a9a8524c83e1b1353a3d6633c76d
cj <thatcat13@gmail.com>
2018-01-18 12:21:08 -0800

    add global array to represent output of all doctors and relocate callback function back into scope of getDoc method

--- js/doc-interface.js
+++ js/doc-interface.js
@@ -12,13 +12,7 @@ $(document).ready(function(){
 
     newRequest.getDoc(function(bacon){
       for (var i = 0; i < bacon.length; i++)
-        $('#results-list').append(`<li>${bacon[0]}</li>`);
-        console.log(bacon[0]);
-        $('#results-list').append(`<li>${bacon[1]}</li>`);
-        $('#results-list').append(`<li>${bacon[2]}</li>`);
-        $('#results-list').append(`<li>${bacon[3]}</li>`);
-        $('#results-list').append(`<li>Accepting New Patients: ${bacon[4]}</li>`);
-        $('#results-list').append(`<li>Website: ${bacon[5]}</li>`);
+        $('#results-list').append(`<li>${bacon[i]}</li>`);
       },  function(error){
       $('#errors').text("There was an error processing your request. Please try again.");
     });//getDoc
--- js/logic.js
+++ js/logic.js
@@ -22,8 +22,9 @@ export class FindDoc {
 
       success: function(response) {
         console.log(response);
+        const finalResults = [];
         for (var i = 0; i < response.data.length; i++){
-          let docComplete = [];
+          const docComplete = [];
           this.docName = response.data[i].profile.last_name + ', ' + response.data[i].profile.first_name;
           this.docStreetAddress = response.data[i].practices[0].visit_address.street;
           this.docCityStateAddress = response.data[i].practices[0].visit_address.city + ', ' + response.data[i].practices[0].visit_address.state;
@@ -36,29 +37,25 @@ export class FindDoc {
 
 
           if (response.data[i].practices[0].accepts_new_patients){
-            this.docNewPatients = 'yes';
+            this.docNewPatients = 'taking new patients';
           } else {
-            this.docNewPatients = 'not at this time';
+            this.docNewPatients = 'not taking any new patients at this time';
           }
 
 
-          docComplete.push(this.docName, this.docStreetAddress, this.docCityStateAddress, this.docPhone, this.docWebsite, this.docNewPatients);
-          console.log(docComplete);
+          docComplete.push(`${this.docName} <br> ${this.docStreetAddress}  <br> ${this.docCityStateAddress}  <br> ${this.docPhone} <br> ${this.docWebsite} <br> ${this.docNewPatients}`);
+          finalResults.push(docComplete);
         }//for loop
 
+        success(finalResults);//callback function named success with ARGUMENT response that hasn't necessarily been grabbed yet from server
 
       },
 
-      // success(response);//callback function named success with ARGUMENT response that hasn't necessarily been grabbed yet from server
 
-      error: function() {
+      error: function(response) {
         error(response);//callback function named error that's a blank template for doing whatever we want when called in frontend
       }
 
     });//ajax
   }//getDoc
 } //FindDoc
-
-// if (response.data[i].practices[0]) {
-//   resultsArray.push(response.data[i].profile.last_name + ', ' + response.data[i].profile.first_name + '<br>' + 'Accepting new patients: ' +  response.data[i].practices[0].accepts_new_patients + '<br>' + 'Address: ' + response.data[i].practices[0].visit_address.street + ' ' + response.data[i].practices[0].visit_address.city + ', ' + response.data[i].practices[0].visit_address.state + '<br>' + 'Phone number: ' + response.data[i].practices[0].phones[0].number + '<br>'+ 'Website: ' + response.data[i].practices[0].website);
-// }
